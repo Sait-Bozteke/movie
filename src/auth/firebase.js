718759dcf8a62f742140c5c9919351aa07c83efb
Sait-Boz <sait.bozteke63@gmail.com>
2023-08-06 09:29:21 +0200

    commit yapildi

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,76 +1,84 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,signOut, updateProfile,onAuthStateChanged, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAUdeSlgbA6GGPXskhCMYSoxXo4nbsbhPg",
+  apiKey: process.env.REACT_APP_apiKey,
   authDomain: "movie-7d7b1.firebaseapp.com",
   projectId: "movie-7d7b1",
   storageBucket: "movie-7d7b1.appspot.com",
   messagingSenderId: "760886502223",
-  appId: "1:760886502223:web:dee649db4c44f86f8b1b3a"
+  appId: "1:760886502223:web:dee649db4c44f86f8b1b3a",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-export const createUser = async (email,password,navigate,displayName) => {
+export const createUser = async (email, password, navigate, displayName) => {
   try {
-    let userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    await updateProfile(auth.currentUser, {displayName: displayName})
-    console.log(userCredential)
-    navigate("/")
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    await updateProfile(auth.currentUser, { displayName: displayName });
+    console.log(userCredential);
+    navigate("/");
+  } catch (error) {
+    alert(error.message);
   }
-  catch(error) {
-   alert(error.message)
-  }
+};
 
-}
-
-export const signIn =async (email,password,navigate)=>{
-  
-  
+export const signIn = async (email, password, navigate) => {
   try {
-    let userCredential = await signInWithEmailAndPassword(auth, email, password);
-    console.log(userCredential)
-    navigate("/")
+    let userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(userCredential);
+    navigate("/");
+  } catch (error) {
+    alert(error.message);
   }
-  catch(error) {
-   alert(error.message)
-  }
-  
-}
+};
 
 export const logOut = () => {
   signOut(auth);
   alert("logged out successfully");
 };
 
-export const userObserver = (setCurrentUser)=>{
+export const userObserver = (setCurrentUser) => {
   onAuthStateChanged(auth, (currentUser) => {
-    if (currentUser) {  
-      setCurrentUser(currentUser)
-      
-      
+    if (currentUser) {
+      setCurrentUser(currentUser);
     } else {
       setCurrentUser(false);
     }
   });
-}
+};
 
-
-export const signUpProvider = (navigate)=> {
+export const signUpProvider = (navigate) => {
   const provider = new GoogleAuthProvider();
   signInWithPopup(auth, provider)
-  .then((result) => {
-    console.log(result);
-    navigate("/")
-
-  }).catch((error) => {
-    console.log(error);
-  });
-}
+    .then((result) => {
+      console.log(result);
+      navigate("/");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
